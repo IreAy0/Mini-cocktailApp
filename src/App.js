483ace47a3707f235d8345ef9425611df849313d
CEIN <cein@Sheriffs-MacBook-Pro.local>
2022-05-04 15:39:13 +0100

    bugs

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ function App() {
          
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="shop" element={<Shop />} />
-      <Route path="cart" element={<Cart />} />
+      <Route path="/shop" element={<Shop />} />
+      <Route path="/cart" element={<Cart />} />
   </Routes>
   
   </BrowserRouter>
